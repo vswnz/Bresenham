98d4b454d1e5f72a6dc601de3068d29cf1102c24
vswnz <vsw@whs.school.nz>
2022-05-11 10:19:54 +1200

    add timing compares for regular maths

--- BresenhamLine.java
+++ BresenhamLine.java
@@ -8,17 +8,30 @@
 public class BresenhamLine
 {
     // instance variables - replace the example below with your own
-    private int x;
+    final boolean VERBOSE=false;
 
     /**
      * Constructor for objects of class Plot
      */
-    public BresenhamLine()
+
+    public BresenhamLine(){
+        Coordinate one=new Coordinate(0,0);
+        Coordinate two=new Coordinate(10,7);
+        BLine(one,two,true);
+    }
+
+    public BresenhamLine(boolean plot){
+        if (plot){
+            Coordinate one=new Coordinate(0,0);
+            Coordinate two=new Coordinate(10,7);
+            BLine(one,two,true);
+        }
+    }
+
+    Board myBoard = new Board();
+
+    public void BLine(Coordinate one, Coordinate two, boolean plot)
     {
-        // initialise instance variables
-        Board myBoard = new Board();
-        Coordinate one=new Coordinate(2,0);
-        Coordinate two=new Coordinate(10,3);
 
         // Parameters as defined byt Bresenham.
         // Yes the names are ugly names, but that is how they are defined.
@@ -26,13 +39,15 @@ public class BresenhamLine
         int b = a - 2*(two.getX()-one.getX());
         int p = a-(two.getX()-one.getX());
         int yStep=1;  // What do we adjust Y by for each move in X?
-        
-        System.out.println("Calculating A is "+a+", B as "+b+"; and p as "+p);
+
+        if (VERBOSE) System.out.println("Calculating A is "+a+", B as "+b+"; and p as "+p);
         int y=one.getY();
         for (int x=one.getX();x<=two.getX();x++){
-            System.out.print("p is now "+p);
-            System.out.println("; Plotting at "+x+","+y);
-            myBoard.plot(new Coordinate(x,y));
+            if (VERBOSE){
+                System.out.print("p is now "+p);
+                System.out.println("; Plotting at "+x+","+y);
+            }
+            if (plot) myBoard.plot(new Coordinate(x,y));
             // Do Bresenham maths.  
             // This version only works with lines sloping up to the right at no more than 45 degrees.
             if (p < 0) { //draw on same line
